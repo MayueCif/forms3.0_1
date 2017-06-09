8c9b8c92bd86738e762a013597f305c3ce3908d1
Myue <mayue8373@live.com>
2017-06-09 09:09:45 +0800

    init code

---
+++ FormsPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace forms3._1
+{
+    public partial class FormsPage : ContentPage
+    {
+
+        public static string ToNative = "to_native";
+
+        void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            MessagingCenter.Send(this, ToNative, "FormsPage To Native");
+        }
+
+        public FormsPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
